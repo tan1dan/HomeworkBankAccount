1df5f5d03d9345d9247a7fd54d777c89309cd85f
Ivan Znak <ivanznak04@gmail.com>
2024-01-02 12:53:27 +0100

    Code was added

--- HomeworkBankAccount/HomeworkBankAccount/ViewController.swift
+++ HomeworkBankAccount/HomeworkBankAccount/ViewController.swift
@@ -8,10 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    class BankAccount {
+        private var balance: Double = 0
+        private let lock = NSRecursiveLock()
+        
+        func deposit(amount: Double){
+            lock.lock()
+            defer { lock.unlock() }
+            balance += amount
+            print("Пополнение счета на сумму: \(amount). Ваш баланс: \(balance)")
+        }
+        
+        func withdraw(amount: Double){
+            lock.lock()
+            defer { lock.unlock() }
+            
+            if balance >= amount {
+                balance -= amount
+                print("На вашем счету осталось: \(balance)")
+            }
+            else {
+                print("На счету недостаточно средств")
+            }
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let account = BankAccount()
+        Thread.detachNewThread {
+            account.deposit(amount: 100)
+        }
+        Thread.detachNewThread {
+            account.withdraw(amount: 120)
+        }
+        Thread.detachNewThread {
+            account.deposit(amount: 300)
+        }
+        Thread.detachNewThread {
+            account.withdraw(amount: 120)
+        }
     }
 
 
