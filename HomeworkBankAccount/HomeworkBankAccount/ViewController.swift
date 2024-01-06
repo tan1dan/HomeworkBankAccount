ddc8c56347b6479fd702ee49c4bd149e6c637732
Ivan Znak <ivanznak04@gmail.com>
2024-01-06 15:46:02 +0100

    New Functional Transfer

--- HomeworkBankAccount/HomeworkBankAccount/ViewController.swift
+++ HomeworkBankAccount/HomeworkBankAccount/ViewController.swift
@@ -10,14 +10,15 @@ import UIKit
 class ViewController: UIViewController {
     
     class BankAccount {
-        private var balance: Double = 0
-        private let lock = NSRecursiveLock()
+        var balance: Double = 0
+        let name: String
+        let lock = NSRecursiveLock()
         
         func deposit(amount: Double){
             lock.lock()
             defer { lock.unlock() }
             balance += amount
-            print("Пополнение счета на сумму: \(amount). Ваш баланс: \(balance)")
+            print("Пополнение счета на сумму: \(amount). Баланс \(name): \(balance)")
         }
         
         func withdraw(amount: Double){
@@ -26,17 +27,38 @@ class ViewController: UIViewController {
             
             if balance >= amount {
                 balance -= amount
-                print("На вашем счету осталось: \(balance)")
+                print("На счету \(name) осталось: \(balance)")
             }
             else {
-                print("На счету недостаточно средств")
+                print("На счету \(name) недостаточно средств")
             }
         }
-        
+        init(name: String) {
+            self.name = name
+        }
     }
+    
+    func transferFromTo(from firstAccount: BankAccount, to secondAccount: BankAccount, amount: Double){
+        if firstAccount.balance >= amount {
+            firstAccount.lock.lock()
+            secondAccount.lock.lock()
+            defer {
+                firstAccount.lock.unlock()
+                secondAccount.lock.unlock()
+            }
+            firstAccount.balance -= amount
+            secondAccount.balance += amount
+            print("С баланса \(firstAccount.name) на баланс \(secondAccount.name) было переведено \(amount)")
+            print("Баланс \(firstAccount.name): \(firstAccount.balance) ")
+            print("Баланс \(secondAccount.name): \(secondAccount.balance)")
+        }
+        else{ print("Недостаточно средств")}
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let account = BankAccount()
+        let account = BankAccount(name: "Vladimir")
+        let secondAccount = BankAccount(name: "Julia")
         Thread.detachNewThread {
             account.deposit(amount: 100)
         }
@@ -49,6 +71,10 @@ class ViewController: UIViewController {
         Thread.detachNewThread {
             account.withdraw(amount: 120)
         }
+        let thread = Thread{
+            self.transferFromTo(from: account, to: secondAccount, amount: 100)
+        }
+        thread.start()
     }
 
 
